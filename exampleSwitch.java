ab471eeaa4d1336c224aad6407fe09b93f50e13a
Swati-purohit123 <101576397+Swati-purohit123@users.noreply.github.com>
2022-03-23 17:02:45 +0530

    Add files via upload

---
+++ exampleSwitch.java
@@ -0,0 +1,20 @@
+
+public class exampleSwitch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int expression= 2;
+		switch (expression) {
+		case 1:
+			System.out.println("case 1");
+		case 2:
+			System.out.println("case 2");
+		case 3:
+			System.out.println("case 3");
+		default:
+			System.out.println("default case");
+		}
+			
+	}
+
+}
